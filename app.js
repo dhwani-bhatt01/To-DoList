c7c7655760520aeea3c04656f97f6779d2429f5f
Dhwani Bhatt <bhattdhwani01@gmail.com>
2021-01-21 13:07:02 +0530

    applied animation

--- app.js
+++ app.js
@@ -2,8 +2,11 @@
 const todoInput = document.querySelector('.todo-input')
 const todoButton = document.querySelector('.todo-button')
 const todoList = document.querySelector('.todo-list')
+const filterOption = document.querySelector('.filter-todo')
 //Event-Listeners
 todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
+filterOption.addEventListener("click", filterTodo);
 //Functions
 function addTodo(event) {
     //Prevent form from submitting 
@@ -31,3 +34,48 @@ function addTodo(event) {
     //Clear Todo INPUT VALUE
     todoInput.value = " ";
 }
+
+    function deleteCheck(e) {
+        const item = e.target;
+        //DELETE TODO
+        if(item.classList[0] === "trash-btn") {
+            const todo = item.parentElement;
+            //ANIMATION
+            todo.classList.add("fall");
+            todo.addEventListener("transitionend", function() {
+                todo.remove();
+            })
+            
+        }
+
+        //CHECK MARK
+        if(item.classList[0] === "complete-btn") {
+            const todo = item.parentElement;
+            todo.classList.toggle("completed");
+        }
+    }
+
+    function filterTodo(e) {
+        const todos = todoList.childNodes;
+        todos.forEach(function(todo){
+            switch(e.target.value){
+                case "all" : 
+                    todo.style.display = "flex";
+                    break;
+                case "completed" :
+                    if(todo.classList.contains("completed")){
+                        todo.style.display = "flex";
+                    } else{
+                        todo.style.display = none;
+                    }
+                    break;
+                case "incompleted" :
+                    if(!todo.classList.contains("completed")){
+                        todo.style.display = "flex";
+                    } else{
+                        todo.style.display = none;
+                    }
+                    break;
+            }
+        });
+    }
